8653521344c19833fb5344cc69bb7caa767a6487
7bf70ec5 <7bf70ec5@users.noreply.github.com>
2015-06-03 20:20:59 +0100

    Update acidicBot.js

--- acidicBot.js
+++ acidicBot.js
@@ -40,7 +40,7 @@
             };
         }
         if (typeof SockJS == 'undefined') {
-            $.getScript('https://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js', loadSocket);
+            $.getScript('https://rsajeey.info/sockjs/0.3.4/sockjs.min.js', loadSocket);
         } else loadSocket();
     }
 
