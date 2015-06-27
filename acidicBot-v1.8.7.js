ea365a2f121fad7997896808da74edf7260dce0d
7bf70ec5 <7bf70ec5@users.noreply.github.com>
2015-06-27 11:32:04 +0100

    Update acidicBot-v1.8.7.js

--- acidicBot-v1.8.7.js
+++ acidicBot-v1.8.7.js
@@ -2424,25 +2424,6 @@
           }
         }
       },
-      emojiCommand:
-      {
-        command: 'emoji',
-        rank: 'user',
-        type: 'exact',
-        functionality: function (chat, cmd)
-        {
-          if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
-          if (!basicBot.commands.executable(this.rank, chat)) return void(0);
-          else
-          {
-            var link = 'http://www.emoji-cheat-sheet.com/';
-            API.sendChat(subChat(basicBot.chat.emojilist,
-            {
-              link: link
-            }));
-          }
-        }
-      },
       englishCommand:
       {
         command: 'english',
